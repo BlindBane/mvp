5ca7f0b66a08a1c02fe0d3ab47d460f480b7bb5e
Aidan Bane <aidanfbane@gmail.com>
2017-03-06 19:17:29 -0800

    server.js and server dir added

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'pug');
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+ res.render('index'); 
+});
+
+var port = 1337;
+app.listen(port, function() { 
+  console.log('Server.js is now listening on port: ' + port + '!');
+});
