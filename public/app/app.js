cf831dea5f3d834c653370d8cebe4411adddf385
Aidan Bane <aidanfbane@gmail.com>
2017-03-06 20:14:43 -0800

    linked pug files

---
+++ public/app/app.js
@@ -0,0 +1,15 @@
+angular.module('app', ['ngRoute'])
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+	.when('/', {
+      templateUrl: '/partials/main',
+      controller: 'mainCtrl'
+    })
+})
+.controller('mainCtrl', function($scope) {
+  $scope.hello = 'Hello, ng!';
+});
+
+
+
+
