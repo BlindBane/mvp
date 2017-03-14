91acf31a1eb7182cf24ec18fe13addfdfcc11285
Aidan Bane <aidanfbane@gmail.com>
2017-03-13 18:46:10 -0700

    Fixed bug in api router

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,14 @@
 'use strict';
 
 var express = require('express');
-
 var router = express.Router();
 
 router.get('/reviews', function(req, res) {
-  res.json('you send a GET request to the mealily Meals api!');
+  res.send('you send a GET request to the mealily Meals api!');
 });
 
 router.post('/reviews', function(req, res) {
-  res.json('you send a POST request to the mealily Meals api!');
+  res.send('you send a POST request to the mealily Meals api!');
 });
 
 module.exports = router;
