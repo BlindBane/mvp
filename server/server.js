8ae3d2ae549f42d84d186d80058b45856cd48d30
Aidan Bane <aidanfbane@gmail.com>
2017-03-13 18:42:33 -0700

    Moved router into seperate module

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var router = require('./api');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -9,17 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static('public'));
 app.use('/node_modules', express.static('node_modules'));
-
-var router = express.Router();
-
-router.get('/reviews', function(req, res) {
-  res.send('you send a GET request to the mealily Meals api!');
-});
-
-router.post('/reviews', function(req, res) {
-  res.send('you send a POST request to the mealily Meals api!');
-});
-
 app.use('/api', router);
 
 app.listen(PORT, function(error) {
